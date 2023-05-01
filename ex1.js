35500e48f0962458a9a4219c0979234dbabe7bb5
omiras <oscar.miras@gmail.com>
2023-05-01 11:09:13 +0000

    test

---
+++ ex1.js
@@ -0,0 +1,10 @@
+/**
+ * Cambia la línea 8 para acumular la suma de cada elemento del array
+ */
+
+const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+numbers.forEach((number) => {
+    console.log(number);
+});
+console.log("Total: ", sum); // Output: 15
