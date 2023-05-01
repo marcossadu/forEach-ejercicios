8afbff536055c7ef286a0e66c29b5c151820c6e8
omiras <oscar.miras@gmail.com>
2023-05-01 11:09:05 +0000

    ex1,2

---
+++ ex2.js
@@ -0,0 +1,10 @@
+/**
+ * Completa el cuerpo de la función del forEach para crear un objeto para cada elemento del array con el nombre de la persona. Crear objetos: https://www.w3schools.com/js/js_objects.asp
+ */
+
+const names = ["John", "Sarah", "Mike"];
+const people = [];
+names.forEach((name) => {
+    // 1 línea de código
+});
+console.log(people); // Output: [{name: "John"}, {name: "Sarah"}, {name: "Mike"}]
